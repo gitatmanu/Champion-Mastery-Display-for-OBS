78b929052ef89ce2cdf762f769da33bab347a848
Manel <manumcraft@gmail.com>
2022-02-26 02:24:23 +0100

    async functions fixed

--- server.js
+++ server.js
@@ -3,22 +3,25 @@ var http = require('http');
 
 var mode = 'custom'; //Select mode [champion in game, custom champion]
 
-var server = http.createServer(function (req, res) {
-    var championId;
-
+var server = http.createServer(async function (req, res) {
     if (req.url == '/') {
         switch(mode) {
             case 'in game':
-                championId = 0; //Get current game stats if there are, if not, take champ from the last game played
+                var championId = 0; 
+                //Get current game stats if there are, if not, take champ from the last game played
                 break;
             case 'custom':
-                championId = lolapi.getChampionIdByName('Annie');
+                var championId = lolapi.getChampionIdByName('Annie');
         }
-        const championMasteryData = lolapi.getChampionMasteryData(championId, 'manufm').then( res => console.log(res.data) );
+        const championMasteryData = await lolapi.getChampionMasteryData(championId, 'manufm').then( res => res.data );
 
-        console.log(championMasteryData);
+        //logo champion
 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        //logo mastery
+
+        
+
+        res.writeHead(200, { 'Content-Type': 'text/json' });
         res.end(JSON.stringify(championMasteryData));
     } else res.end('Invalid Request!');
 });
